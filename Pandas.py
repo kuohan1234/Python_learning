dad1ce5de0835ea82c0172531c65ff0bd83c3b0f
kuohan <james29802327@gmail.com>
2025-02-01 21:10:36 +0800

    update V2

--- Pandas.py
+++ Pandas.py
@@ -5,5 +5,7 @@ df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
 
     # Calculate the sum of each column 
 column_sums = df.sum()
+
+################################################################
     
 print(column_sums)
