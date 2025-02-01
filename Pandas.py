cd78cf7e1a2f8e0d3e33673ca756fad605187124
kuohan <james29802327@gmail.com>
2025-02-01 20:47:47 +0800

    Create a new python pandas practice

---
+++ Pandas.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+
+    # Create a DataFrame from a dictionary
+df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
+
+    # Calculate the sum of each column 
+column_sums = df.sum()
+    
+print(column_sums)
